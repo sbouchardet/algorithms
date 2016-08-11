062932829aee0e24f8ea2c39ba5d0782e328b7dd
Susana Bouchardet <susana.bouchardet@bouchardet.local>
2016-08-11 00:21:59 -0300

    criação da estrutura para ordenação

--- sort/bubble_sort.cpp
+++ sort/bubble_sort.cpp
@@ -12,16 +12,35 @@ public:
     this->value=val;
     this->next=NULL;
     this->before=NULL;
-    
+
   }
+
+  void append(int val){
+    node * new_node =  new node(val);
+    node * last = this;
+    while(last->next != NULL) last = last->next;
+    last->next = new_node;
+    new_node->before=last;
+  }
+
+  void print_all(){
+    node *k=this;
+    while (k != NULL) {
+      cout<< k->value<<endl;
+      k=k->next;
+    }
+  }
+
+
 };
 
 
 int main(){
-  node na(20);
-  na.value = 10;
+
   node *n = new node(2);
-  n->value=5;
-  cout<< n->value<<" e "<<na.value<<endl;
+  n->append(4);
+  n->append(3);
+  n->append(6);
+  n->print_all();
   return 0;
 }
