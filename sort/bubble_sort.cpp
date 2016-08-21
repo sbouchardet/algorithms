6b05b31b23531337800106d9115db07cb8abcc88
Susana Bouchardet <susana.bouchardet@bouchardet.local>
2016-08-21 17:32:34 -0300

    bubble_sort: finished!

--- sort/bubble_sort.cpp
+++ sort/bubble_sort.cpp
@@ -1,46 +1,51 @@
+#include "extruct.cpp"
+
 #include <iostream>
 
 using namespace std;
 
-class node{
+class bubble_sort{
+
+private:
+  static void change(node * n1, node *n2){
+    float *a = n1->get_info();
+    n1->set_info(n2->get_info());
+    n2->set_info(a);
+  }
 public:
-  float value;
-  float index;
-  node *next;
-  node *before;
-  node(float val){
-    this->value=val;
-    this->next=NULL;
-    this->before=NULL;
-
-  }
-
-  void append(int val){
-    node * new_node =  new node(val);
-    node * last = this;
-    while(last->next != NULL) last = last->next;
-    last->next = new_node;
-    new_node->before=last;
-  }
-
-  void print_all(){
-    node *k=this;
-    while (k != NULL) {
-      cout<< k->value<<endl;
-      k=k->next;
+  static void sort(node * r){
+    node * i = r;
+    node *j=NULL;
+    float vi=0;
+    float vj=0;
+    while(i->next!=NULL){
+      j=i->next;
+      while(j->next!=NULL){
+        vi = i->get_value();
+        vj = j->get_value();
+        if(vi>vj) change(i,j);
+        j=j->next;
+      }
+      i=i->next;
     }
   }
 
 
 };
 
-
 int main(){
-
-  node *n = new node(2);
+  node * n = new node(-1);
+  n->append(1);
+  n->append(5);
   n->append(4);
-  n->append(3);
-  n->append(6);
+  n->append(2);
+  n->append(5);
+
   n->print_all();
+cout<<"\nSorted:"<<endl;
+bubble_sort::sort(n);
+n->print_all();
+
+
   return 0;
 }
