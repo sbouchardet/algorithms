f3435916f0eae008ebf7ea28f0126720ae8aee59
susana.bouchardet <susana.bouchardet@corp.globo.com>
2018-04-10 20:27:19 -0300

    quicksort

--- sort/quicksort.go
+++ sort/quicksort.go
@@ -1,3 +1,39 @@
 package sort
 
-// TODO
+import "math/rand"
+
+// Quicksort sort
+func Quicksort(input []int) []int {
+	n := len(input)
+
+	if n <= 1 {
+		return input
+	}
+
+	p := input[rand.Intn(n)]
+	L, P, R := partition(input, p)
+
+	LSorted := Quicksort(L)
+	RSorted := Quicksort(R)
+
+	concatAux := append(LSorted, P...)
+	return append(concatAux, RSorted...)
+
+}
+
+func partition(vector []int, pivo int) ([]int, []int, []int) {
+	L, P, R := []int{}, []int{}, []int{}
+	for i := 0; i < len(vector); i++ {
+		if vector[i] > pivo {
+			R = append(R, vector[i])
+		}
+		if vector[i] < pivo {
+			L = append(L, vector[i])
+		}
+		if vector[i] == pivo {
+			P = append(P, vector[i])
+		}
+	}
+
+	return L, P, R
+}
