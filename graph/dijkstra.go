f530ee8aabaa0d9a979115ba42006f4d7fca6fcc
Susana Bouchardet <susana.bouchardet@gmail.com>
2018-05-22 21:09:22 -0300

    includes dijkstra algorithm

---
+++ graph/dijkstra.go
@@ -0,0 +1,12 @@
+package graph
+
+type piFunc struct {
+	n  *Node
+	pi int
+}
+
+// FindBestPath Using Dijskstr algorithms to find bestPath
+func FindBestPath(root *Node) []*Edge {
+	//TODO: implement algorithm.
+	return nil
+}
