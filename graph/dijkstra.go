e19f10933a61cea6bba15bbbd807609f40247566
Susana Bouchardet <susana.bouchardet@gmail.com>
2018-05-22 21:19:07 -0300

    starts djikstra implementation

--- graph/dijkstra.go
+++ graph/dijkstra.go
@@ -10,3 +10,16 @@ func FindBestPath(root *Node) []*Edge {
 	//TODO: implement algorithm.
 	return nil
 }
+
+func getBestEdge(piVector []*piFunc) (*piFunc, int, []*piFunc) {
+	var min *piFunc
+	iMin := -1
+	for i, value := range piVector {
+		if min != nil && value.pi < min.pi {
+			min = value
+			iMin = i
+		}
+	}
+	piVectorRemoved := append(piVector[:iMin], piVector[iMin+1:]...)
+	return min, iMin, piVectorRemoved
+}
